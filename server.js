d64c9571d7c8c852959c2cf53a5cd7e0c9d7b5ee
Dann Blair <dann@bitspacedevelopment.com>
2016-04-15 13:29:21 -0500

    Added a cursor and mouse events. Trying to get obj files working

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// Super simple server script for local dev
+
 var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname)).listen(8080, function(){
